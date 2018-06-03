68fba228b98ec8c731e4329c17960aca491273f3
Raiet Intraub <raietsaeed@gmail.com>
2018-06-03 22:01:35 +0000

    automatically backed up by learn

--- db/migrate/create_preferences.rb
+++ db/migrate/create_preferences.rb
@@ -1,7 +1,10 @@
 class CreatePreferences < ActiveRecord::Migration
   def change
     create_table :preferences do |t|
-      t.string :name
+      t.string :artist_sort_order
+      t.string :song_sort_order
+      t.boolean :allow_create_songs
+      t.boolean :allow_create_artists
 
       t.timestamps null: false
     end
