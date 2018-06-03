31b597fb2005644393b682c977d152b147fbae5c
Raiet Intraub <raietsaeed@gmail.com>
2018-06-03 22:15:05 +0000

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,5 +1,6 @@
 class ArtistsController < ApplicationController
   def index
+    @preference = Preference.last
     @artists = Artist.all
   end
 
@@ -8,7 +9,11 @@ class ArtistsController < ApplicationController
   end
 
   def new
-    @artist = Artist.new
+    if Preference.last.allow_create_artists == true
+      @artist = Artist.new
+    else
+      redirect_to artists_path
+    end
   end
 
   def create
--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,6 @@
 class SongsController < ApplicationController
   def index
+    @preference = Preference.last
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
@@ -25,7 +26,12 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if Preference.last.allow_create_songs == true
+      @song = Song.new
+    else
+      redirect_to songs_path
+    end
+
   end
 
   def create
@@ -67,4 +73,3 @@ class SongsController < ApplicationController
     params.require(:song).permit(:title, :artist_name)
   end
 end
-
